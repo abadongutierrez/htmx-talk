6f0aa5e2703cbdedbfc3444b77fc47b801fe848f
abadongutierrez <abadon.gutierrez@gmail.com>
2024-08-19 19:58:28 -0600

    first commit

---
+++ src/main/java/com/jabaddon/learning/lightningtalk/htmx_talk/HtmxTalkApplication.java
@@ -0,0 +1,13 @@
+package com.jabaddon.learning.lightningtalk.htmx_talk;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HtmxTalkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HtmxTalkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jabaddon/learning/lightningtalk/htmx_talk/HtmxTalkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jabaddon.learning.lightningtalk.htmx_talk;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HtmxTalkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
