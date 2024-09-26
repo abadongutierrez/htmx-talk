55e1eec91b246e342f99294116deef5643f33765
abadongutierrez <abadon.gutierrez@gmail.com>
2024-09-26 08:35:46 -0600

    progress

--- src/main/java/com/jabaddon/learning/lightningtalk/htmx_talk/FragmentsController.java
+++ src/main/java/com/jabaddon/learning/lightningtalk/htmx_talk/FragmentsController.java
@@ -1,8 +1,15 @@
 package com.jabaddon.learning.lightningtalk.htmx_talk;
 
+import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.Objects;
+import java.util.stream.Collectors;
 
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -14,12 +21,27 @@ import org.springframework.web.servlet.ModelAndView;
 @Controller
 @RequestMapping("/fragments")
 public class FragmentsController {
+    private static final Logger logger = LoggerFactory.getLogger(FragmentsController.class);
 
     @GetMapping("/say-hello")
     public ModelAndView sayHello(@RequestParam String name, @RequestParam(required=false) String lastName) {
+        List<String> values = new ArrayList<>();
+        values.add(name);
+        values.add(lastName);
+        String wholeName = values.stream().filter(Objects::nonNull).collect(Collectors.joining(" "));
         Map<String, String> model = new HashMap<>();
-        model.put("wholeName", name + (lastName != null ? " " + lastName : ""));
+        model.put("wholeName", StringUtils.isBlank(wholeName) ? "?" : wholeName);
         return new ModelAndView("fragments/say-hello", model);
+    }
+
+    @GetMapping("/echo-name-email")
+    public ModelAndView echoNameEmail(@RequestParam String name, @RequestParam(required=false) String email) {
+        logger.info("name: {}, email: {}", name, email);
+        Map<String, Object> model = new HashMap<>();
+        model.put("name", name);
+        model.put("email", email);
+        model.put("isEmail", !StringUtils.isBlank(email));
+        return new ModelAndView("fragments/echo-name-email", model);
     }
 
     @GetMapping("/basics/1")
